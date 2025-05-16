ca6204ecd14260971f80a8d904379436bc0d5d54
GMSDaniil <daniil.cherepko@gmail.com>
2025-05-16 13:07:43 +0200

    Changed hosting

--- lib/core/constants/api_urls.dart
+++ lib/core/constants/api_urls.dart
@@ -1,6 +1,6 @@
 class ApiUrls {
 
-  static const baseURL = 'https://unimapback-production.up.railway.app/';
+  static const baseURL = 'http://188.245.63.53:5000/';
   static const register = '${baseURL}api/Users/register';
   static const login = '${baseURL}api/Users/login';
   static const refreshToken = '${baseURL}api/Tokens/refreshToken';
